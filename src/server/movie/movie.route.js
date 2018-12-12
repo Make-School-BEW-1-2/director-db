1ce23113fd2a1331bb2ba3d2ceb6de6897880065
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-12 02:32:49 -0800

    Create post route for a new movie

--- src/server/movie/movie.route.js
+++ src/server/movie/movie.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Movie = require('./movie.model');
+const Director = require('../director/director.model');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -16,6 +17,23 @@ router.get('/:movieId', (req, res) => {
       } else {
         res.status(404).send('Movie not found');
       }
+    }).catch((err) => {
+      console.error(err);
+      res.status(400).send(err);
+    });
+});
+
+// POST: Create a new movie
+router.post('/new', (req, res) => {
+  const newMovie = new Movie(req.body);
+  Director.findById(req.params.directorId)
+    .then((director) => {
+      newMovie.directors.unshift(director);
+      newMovie.save();
+      director.movies.unshift(newMovie);
+      director.markModified('movies');
+      director.save();
+      res.status(200).send(director);
     }).catch((err) => {
       console.error(err);
       res.status(400).send(err);
