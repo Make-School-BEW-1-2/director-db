e796ba8c560a60aa2b3e0e9e40fbd4d4c24e7bd8
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-12 11:08:14 -0800

    Create route for removing a movie

--- src/server/movie/movie.route.js
+++ src/server/movie/movie.route.js
@@ -69,4 +69,36 @@ router.put('/:movieId/:newDirectorId', (req, res) => {
     });
 });
 
+// DELETE: Removes a single movie
+router.delete('/:movieId', (req, res) => {
+  // find the movie to delete
+  Movie.findById(req.params.movieId)
+    .populate('directors')
+    .then((movie) => {
+      // if a movie was found
+      if (movie) {
+        for (let i = 0; i < movie.directors.length; i += 1) {
+          const editMovie = movie;
+          // If I edit a reference to model
+          //  and save the model that holds the reference will the model be changed as well?
+          editMovie.directors[i].movies =
+            movie.directors[i].movies.filter(filterMovie => filterMovie._id !== movie._id);
+          movie.markModified('directors');
+          movie.save();
+        }
+        // remove the movie
+        return Movie.findOneAndRemove({
+          id: movie._id
+        });
+      }
+      throw new Error('Could not find movie');
+    }).then((movie) => {
+      res.status(200).send(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(400).send(err);
+    });
+});
+
 module.exports = router;
